8d8830f8d1d1b76d77fb13b7f0dd79a455eff204
sshuvoo <safayanshuvo@gmail.com>
2024-06-14 12:21:38 +0600

    Basic Setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from "tailwindcss";
+const {
+   default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette')
+
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+   content: [
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+   ],
+   darkMode: 'class',
+   theme: {
+      extend: {},
+   },
+   plugins: [addVariablesForColors],
+}
+export default config
+
+function addVariablesForColors({ addBase, theme }: any) {
+   let allColors = flattenColorPalette(theme('colors'))
+   let newVars = Object.fromEntries(
+      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+   )
+   addBase({
+      ':root': newVars,
+   })
+}
